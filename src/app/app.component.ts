435aec9a0ed920ce70901d4be9b4266e5db76217
Angel Motta <angel.motta@utec.edu.pe>
2024-07-10 19:51:38 -0500

    feat: getters to update filtered data - edge cases

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,44 +13,66 @@ export class AppComponent {
     { id: 2, wish: 'Learn Spring Boot', isComplete: false },
     { id: 3, wish: 'Get a cup of coffee', isComplete: true },
   ];
-  filteredList = this.wishList;
-
+  // filteredList = this.wishList;
+  get filteredList() {
+    if (this.filterStatus === '0') {
+      // All selected
+      return this.wishList;
+    } else if (this.filterStatus === '1') {
+      // Unfulfilled selected
+      return this.wishList.filter((wishObj) => wishObj.isComplete === false);
+    } else if (this.filterStatus === '2') {
+      // fullfilled selected
+      return this.wishList.filter((wishObj) => wishObj.isComplete === true);
+    } else {
+      // All elements (should not happen)
+      console.log(`filtered list: something went wrong`);
+      return this.wishList;
+    }
+  }
   newWish = '';
   filterStatus = '0';
 
   toggleWish(wishObj: WishItem) {
     console.log(`clicked`);
-    console.log(`init`);
     console.log(wishObj);
     wishObj.isComplete = !wishObj.isComplete;
     console.log(wishObj);
     console.log(`end`);
+    // Update filtered elements
+    // this.handleFilterChange();
   }
 
   addNewWish() {
+    if (!this.newWish) {
+      alert(`wish can not be blank`);
+      return;
+    }
     const newUserWish: WishItem = {
       wish: this.newWish,
       isComplete: false,
     };
     this.wishList.push(newUserWish);
     this.newWish = '';
+    // Update filtered elements
+    // this.handleFilterChange();
   }
 
-  handleFilterChange() {
-    console.log(`filter changed: ${this.filterStatus}`);
-    if (this.filterStatus === '0') {
-      // All selected
-      this.filteredList = this.wishList;
-    } else if (this.filterStatus === '1') {
-      // Unfulfilled selected
-      this.filteredList = this.wishList.filter(
-        (wishObj) => wishObj.isComplete === false
-      );
-    } else if (this.filterStatus === '2') {
-      // fullfilled selected
-      this.filteredList = this.wishList.filter(
-        (wishObj) => wishObj.isComplete === true
-      );
-    }
-  }
+  // handleFilterChange() {
+  //   console.log(`filter changed: ${this.filterStatus}`);
+  //   if (this.filterStatus === '0') {
+  //     // All selected
+  //     this.filteredList = this.wishList;
+  //   } else if (this.filterStatus === '1') {
+  //     // Unfulfilled selected
+  //     this.filteredList = this.wishList.filter(
+  //       (wishObj) => wishObj.isComplete === false
+  //     );
+  //   } else if (this.filterStatus === '2') {
+  //     // fullfilled selected
+  //     this.filteredList = this.wishList.filter(
+  //       (wishObj) => wishObj.isComplete === true
+  //     );
+  //   }
+  // }
 }
