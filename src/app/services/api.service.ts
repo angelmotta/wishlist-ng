e16c54c0910cb5eda9929ff956dc2f8e881b435c
Angel Motta <angel.motta@utec.edu.pe>
2024-07-12 15:00:05 -0500

    feat: get data from API using httpclient

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,13 +1,34 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { WishItem } from '../models/wishItem';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ApiService {
+  baseUrl = 'http://localhost:3000/wishes';
   constructor(private http: HttpClient) {}
 
+  private getStandardOption(): any {
+    return {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+      }),
+    };
+  }
+
   getAllWishes() {
-    return this.http.get('assets/wishes.json');
+    // return this.http.get('assets/wishes.json');
+    let httpOptions = this.getStandardOption();
+    return this.http.get(this.baseUrl, httpOptions);
+  }
+
+  addWish(wishObj: WishItem) {
+    let httpOptions = this.getStandardOption();
+    // httpOptions.headers = httpOptions.headers.set(
+    //   'Authorization',
+    //   'Bearer xyz'
+    // );
+    return this.http.post(this.baseUrl, wishObj);
   }
 }
