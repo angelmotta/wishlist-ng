1d157ca1f32c8a1520d12c9c7935d09b23b303e5
Angel Motta <angel.motta@utec.edu.pe>
2024-07-13 17:22:02 -0500

    feat: form validators

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -8,13 +8,18 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 export class ContactComponent {
   contactFormGroup = new FormGroup({
-    senderName: new FormControl(''),
-    senderEmail: new FormControl(''),
-    senderMessage: new FormControl(''),
+    senderName: new FormControl('', Validators.required),
+    senderEmail: new FormControl('', [Validators.required, Validators.email]),
+    senderMessage: new FormControl('', [
+      Validators.required,
+      Validators.minLength(10),
+    ]),
   });
 
   handleSubmit() {
     console.log(`Handle subit form event`);
+    console.log(`Invalid status: ${this.contactFormGroup.invalid}`);
+
     console.log(this.contactFormGroup.value);
   }
 }
